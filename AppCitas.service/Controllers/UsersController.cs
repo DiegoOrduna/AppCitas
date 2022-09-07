cc0bcde668a4b95a68464eb96b39d61afee50eb1
DiegoOrduna <odiegotoni@gmail.com>
2022-09-07 07:48:03 -0500

    Adding DB and a new controller for Users

---
+++ AppCitas.service/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using AppCitas.service.Data;
+using AppCitas.service.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
+
+namespace AppCitas.service.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        return _context.Users.ToList();
+    }
+}
