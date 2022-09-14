0f7cf4fc7178ffec8727dcec112d34a597df901a
DiegoOrduna <odiegotoni@gmail.com>
2022-09-14 00:30:14 -0500

    Updated DB w hash. Added BaseAPIController n AccountController

---
+++ AppCitas.service/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using AppCitas.service.Data;
+using AppCitas.service.Entities;
+using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AppCitas.service.Controllers;
+
+public class AccountController : BaseApiController
+{
+    private readonly DataContext _context;
+    public AccountController(DataContext context)
+    {
+        _context = context;
+
+    }
+    [HttpPost("register")]
+    public async Task<ActionResult<AppUser>> Register(string username, string password)
+    {
+        using var hmac = new HMACSHA512();
+        var user = new AppUser
+        {
+            UserName = username,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            PasswordSalt = hmac.Key
+        };
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return user;
+    }
+}
--- AppCitas.service/Controllers/UsersController.cs
+++ AppCitas.service/Controllers/UsersController.cs
@@ -2,13 +2,11 @@
 using AppCitas.service.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using SQLitePCL;
+using Microsoft.EntityFrameworkCore;
 
 namespace AppCitas.service.Controllers;
 
-[Route("api/[controller]")]
-[ApiController]
-public class UsersController : ControllerBase
+public class UsersController : BaseApiController
 {
     private readonly DataContext _context;
     public UsersController(DataContext context)
@@ -18,15 +16,15 @@ public class UsersController : ControllerBase
     
     //GET api/users
     [HttpGet]
-    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        return _context.Users.ToList();
+        return await _context.Users.ToListAsync();
     }
 
     //GET api/users/id
     [HttpGet("{id}")]
-    public ActionResult<AppUser> GetUserById(int id)
+    public async Task<ActionResult<AppUser>> GetUserById(int id)
     {
-        return _context.Users.Find(id);
+        return await _context.Users.FindAsync(id);
     }
 }
