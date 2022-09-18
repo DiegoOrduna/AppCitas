33b0a4d5dcfdf630a9e8b9401b3a6027fb9e49b7
DiegoOrduna <odiegotoni@gmail.com>
2022-09-18 18:54:58 -0500

    account controller

--- AppCitas.service/Controllers/AccountController.cs
+++ AppCitas.service/Controllers/AccountController.cs
@@ -20,7 +20,7 @@ public class AccountController : BaseApiController
     [HttpPost("register")]
     public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
     {
-        if(await UserExists(registerDto.Username)) 
+        if (await UserExists(registerDto.Username))
             return BadRequest("username is already taken");
 
         using var hmac = new HMACSHA512();
@@ -37,10 +37,29 @@ public class AccountController : BaseApiController
         return user;
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    {
+        var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+        if (user == null)
+            return Unauthorized("Invalid user or password");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i])
+                return Unauthorized("Invalid user or password");
+        }
+        return user;
+    }
+
     #region Private Methods
     private async Task<bool> UserExists(string username)
     {
-        return await _context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower()); 
+        return await _context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
     }
     #endregion
 }
